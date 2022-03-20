1acf991ed2aee5714a486079b1f4fc122c1488d4
yuerwen <2660955327@qq.com>
2022-03-21 02:55:53 +0800

    Initial commit

---
+++ cases/test.py
@@ -0,0 +1,10 @@
+"""
+@Project ：Auto_hytest 
+@Author : 文跃锐（yuerwen）
+@University:东莞理工学院
+@Time   : 2022/03/21
+@File   :test.py
+"""
+from selenium import webdriver
+
+webDriver = webdriver.Chrome()
