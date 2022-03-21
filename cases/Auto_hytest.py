79d344911ae24d5430e02613ad317f2b32006a1e
yuerwen <2660955327@qq.com>
2022-03-22 01:28:37 +0800

    Merge remote-tracking branch 'github/master'

--- cases/Auto_hytest.py
+++ cases/Auto_hytest.py
@@ -8,6 +8,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from hytest import STEP,INFO,CHECK_POINT
+from lib.common import *
 import time
 
 class UI_0101:
@@ -15,30 +16,12 @@ class UI_0101:
 	
 	def teststeps(self):
 		
-		STEP(1,'打开浏览器，登录网站')
-		# 创建一个webdriver 实例对象，指明使用Chrome浏览器驱动
-		# 加上参数，禁止 chromedriver 日志写屏
-		options = webdriver.ChromeOptions()
-		options.add_experimental_option(
-			'excludeSwitches', ['enable-logging'])
-		
-		# 这里指定 options 参数
-		webDriver = webdriver.Chrome(options=options)
-		
-		# 设置等等时间
-		webDriver.implicitly_wait(5)
-		
-		# 根据webdriver对象的get方法 打开指定的web地址
-		web_file = r'http://127.0.0.1/mgr/sign.html'
-		webDriver.get(web_file)
-		
-		# 输入用户名和密码
-		webDriver.find_element(By.ID, 'username').send_keys('byhy')
-		webDriver.find_element(By.ID, 'password').send_keys('88888888')
-		# 点击按钮
-		webDriver.find_element(By.CLASS_NAME, 'btn-primary').click()
+		STEP(1, '打开浏览器，登录网站')
+		open_browser()
+		mgr_login()
 		
 		STEP(2,'获取左侧菜单信息')
+		webDriver = GSTORE['wd']
 		
 		sidebar_menu = webDriver.find_element(By.CLASS_NAME, 'sidebar-menu')
 		span_lists = sidebar_menu.find_elements(By.TAG_NAME, 'span')
@@ -54,30 +37,14 @@ class UI_0102:
 	name = '添加客户信息 - UI_0102'
 	
 	def teststeps(self):
-		STEP(1,'打开浏览器，登录网站')
-		# 创建一个webdriver 实例对象，指明使用Chrome浏览器驱动
-		# 加上参数，禁止 chromedriver 日志写屏
-		options = webdriver.ChromeOptions()
-		options.add_experimental_option(
-			'excludeSwitches', ['enable-logging'])
 		
-		# 这里指定 options 参数
-		webDriver = webdriver.Chrome(options=options)
-		
-		# 设置等等时间
-		webDriver.implicitly_wait(5)
-		
-		# 根据webdriver对象的get方法 打开指定的web地址
-		web_file = r'http://127.0.0.1/mgr/sign.html'
-		webDriver.get(web_file)
-		
-		# 输入用户名和密码
-		webDriver.find_element(By.ID, 'username').send_keys('byhy')
-		webDriver.find_element(By.ID, 'password').send_keys('88888888')
-		# 点击按钮
-		webDriver.find_element(By.CLASS_NAME, 'btn-primary').click()
+		STEP(1, '打开浏览器，登录网站')
+		open_browser()
+		mgr_login()
 		
 		STEP(2,'点击左侧菜单客户按钮')
+		webDriver = GSTORE['wd']
+		
 		webDriver.find_element(
 			By.CLASS_NAME, 'add-one-area'
 		).click()
---
+++ lib/common.py
@@ -0,0 +1,49 @@
+"""
+@Project ：Auto_hytest 
+@Author : 文跃锐（yuerwen）
+@University:东莞理工学院
+@Time   : 2022/03/22
+@File   :common.py
+"""
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from hytest import *
+
+
+def open_browser():
+	'''
+	打开浏览器函数
+	:return: 空
+	'''
+	
+	# 创建一个webdriver 实例对象，指明使用Chrome浏览器驱动
+	# 加上参数，禁止 chromedriver 日志写屏
+	options = webdriver.ChromeOptions()
+	options.add_experimental_option(
+		'excludeSwitches', ['enable-logging'])
+	
+	# 这里指定 options 参数
+	webDriver = webdriver.Chrome(options=options)
+	# 设置等等时间
+	webDriver.implicitly_wait(5)
+	
+	# 存储 全局共享 数据
+	GSTORE['wd'] = webDriver
+
+def mgr_login():
+	'''
+	管理员登录
+	:return:
+	'''
+	
+	webDriver = GSTORE['wd']
+	
+	# 根据webdriver对象的get方法 打开指定的web地址
+	web_file = r'http://127.0.0.1/mgr/sign.html'
+	webDriver.get(web_file)
+	
+	# 输入用户名和密码
+	webDriver.find_element(By.ID, 'username').send_keys('byhy')
+	webDriver.find_element(By.ID, 'password').send_keys('88888888')
+	# 点击按钮
+	webDriver.find_element(By.CLASS_NAME, 'btn-primary').click()
