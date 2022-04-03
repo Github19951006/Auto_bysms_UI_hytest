4b3af6f8ca5dd091ddef90ef15ba5e4bb9064fa8
yuerwen <2660955327@qq.com>
2022-04-03 15:29:16 +0800

    Merge remote-tracking branch 'github/master'

--- lib/common.py
+++ lib/common.py
@@ -5,7 +5,7 @@
 @Time   : 2022/03/22
 @File   :common.py
 """
-from selenium immv ,port webdriver
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from hytest import *
 
@@ -14,7 +14,24 @@ def open_browser():
 	'''
 	打开浏览器函数
 	:return: 空
-	''' hn ujk  j mgr_login():
+	'''
+	
+	# 创建一个webdriver 实例对象，指明使用Chrome浏览器驱动
+	# 加上参数，禁止 chromedriver 日志写屏
+	INFO('打开浏览器')
+	options = webdriver.ChromeOptions()
+	options.add_experimental_option(
+		'excludeSwitches', ['enable-logging'])
+	
+	# 这里指定 options 参数
+	webDriver = webdriver.Chrome(options=options)
+	# 设置等等时间
+	webDriver.implicitly_wait(5)
+	
+	# 存储 全局共享 数据
+	GSTORE['webDriver'] = webDriver
+
+def mgr_login():
 	'''
 	管理员登录
 	:return:空
